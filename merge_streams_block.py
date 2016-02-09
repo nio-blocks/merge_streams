5e10526f9405f7a274f75d771cc19b474c618aa8
Chris Brackert <cbrackert@gmail.com>
2016-02-08 18:34:30 -0800

    Refactor block

--- merge_streams_block.py
+++ merge_streams_block.py
@@ -20,58 +20,35 @@ class MergeStreams(Block):
 
     def __init__(self):
         super().__init__()
-        self._signal_1 = None
-        self._signal_2 = None
-        self._signal_expiration_job_input_1 = None
-        self._signal_expiration_job_input_2 = None
+        self._signals = { "input_1": {}, "input_2": {} }
+        self._expiration_jobs = { "input_1": None, "input_2": None }
 
     def process_signals(self, signals, input_id='default'):
+        merged_signals = []
         for signal in signals:
-            if input_id == "input_1":
-                #TODO: deepcopy signals?
-                self._signal_1 = signal
-                if self._signal_2:
-                    self.notify_signals(
-                        [self._merge_signals(signal, self._signal_2)])
-                    if self.notify_once:
-                        self._signal_1 = None
-                        self._signal_2 = None
-            else:
-                self._signal_2 = signal
-                if self._signal_1:
-                    self.notify_signals(
-                        [self._merge_signals(signal, self._signal_1)])
-                    if self.notify_once:
-                        self._signal_1 = None
-                        self._signal_2 = None
+            self._signals[input_id] = signal
+            if self._signals["input_1"] and self._signals["input_2"]:
+                merged_signals.append(self._merge_signals())
         if self.expiration:
             self._schedule_signal_expiration_job(input_id)
+        if merged_signals:
+            self.notify_signals(merged_signals)
 
-    def _merge_signals(self, signal_1, signal_2):
-        merged_signal = signal_1.to_dict()
-        merged_signal.update(signal_2.to_dict())
+    def _merge_signals(self):
+        """ Merge signals 1 and 2 and clear from memory if only notify once """
+        merged_signal = self._signals["input_1"].to_dict()
+        merged_signal.update(self._signals["input_2"].to_dict())
+        if self.notify_once:
+            self._signals["input_1"] = {}
+            self._signals["input_2"] = {}
         return Signal(merged_signal)
 
     def _schedule_signal_expiration_job(self, input_id):
-        if input_id == "input_1":
-            if self._signal_expiration_job_input_1:
-                self._signal_expiration_job_input_1.cancel()
-            self._signal_expiration_job_input_1 = Job(
-                self._signal_expiration_job,
-                self.expiration,
-                False,
-                input_id)
-        else:
-            if self._signal_expiration_job_input_2:
-                self._signal_expiration_job_input_2.cancel()
-            self._signal_expiration_job_input_2 = Job(
-                self._signal_expiration_job,
-                self.expiration,
-                False,
-                input_id)
+        """ Schedule expiration job, cancelling existing job first """
+        if self._expiration_jobs[input_id]:
+            self._expiration_jobs[input_id].cancel()
+        self._expiration_jobs[input_id] = Job(
+            self._signal_expiration_job, self.expiration, False, input_id)
 
     def _signal_expiration_job(self, input_id):
-        if input_id == "input_1":
-            self._signal_1 = None
-        else:
-            self._signal_2 = None
+        self._signals[input_id] = {}
